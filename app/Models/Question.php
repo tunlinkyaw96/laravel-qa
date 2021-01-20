7668b07ae401308b4308713b3a140a2a916603e5
tunlinkyaw96 <minion22001122@gmail.com>
2021-01-20 17:12:18 +0630

    after Lesson 8 restore

---
+++ app/Models/Question.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title','body'];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title']=$value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         \App\Models\User::factory(3)->create()->each(function ($u){
+             $u->questions()
+                 ->saveMany(
+                     \App\Models\Question::factory(10)->make()
+                 );
+         });
+
+    }
+}
